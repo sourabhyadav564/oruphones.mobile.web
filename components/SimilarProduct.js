b78601966672f904799d714153ff5b9849ac4f3f
Aman Khandelwal <aman@zenro.co.jp>
2022-07-01 17:23:34 +0530

    removing deplicates from similar products

--- components/SimilarProduct.js
+++ components/SimilarProduct.js
@@ -13,7 +13,7 @@ import { useRecoilState } from "recoil";
 
 function SimilarProduct({ data, otherVendorData }) {
   const { selectedSearchCity } = useAuthState();
-  const [similar_listings, setSimilar_listings] = useState();
+  let [similar_listings, setSimilar_listings] = useState();
 
   console.log("data from similar products --> N", data);
   console.log("data from similar products -- > Y", otherVendorData);
@@ -29,25 +29,33 @@ function SimilarProduct({ data, otherVendorData }) {
       deviceStorage: [],
       maxsellingPrice: 200000,
       minsellingPrice: 0,
-      verified: ""
+      verified: "",
     };
-    fetchSimilarProducts(payLoad, Cookies.get("info") || "Guest").then((response) => {
-      console.log(
-        "response",
-        response?.dataObject?.otherListings.filter((items) => {
-          return items.listingId !== data.listingId;
-        })
-      );
-      setSimilar_listings(
-        response?.dataObject?.otherListings.filter((items) => {
-          return items.listingId !== data.listingId;
-        })
-      );
-    });
+    fetchSimilarProducts(payLoad, Cookies.get("info") || "Guest").then(
+      (response) => {
+        console.log(
+          "response from similar",
+          response?.dataObject?.otherListings.filter((items) => {
+            return items.listingId !== data.listingId;
+          })
+        );
+        setSimilar_listings(
+          response?.dataObject?.otherListings.filter((items) => {
+            return items.listingId !== data.listingId;
+          })
+        );
+      }
+    );
   }, [data?.make, data?.marketingName]);
 
   const [product, setProductsData] = useRecoilState(otherVendorDataState);
-  console.log("product from similar page state variable----->", product);
+  // console.log("product from similar page state variable----->", product);
+
+  similar_listings = similar_listings?.filter((item) => {
+    return item.listingId != otherVendorData[0]?.listingId || data?.listingId
+  });
+
+  console.log("similar_listings from similar products -->", similar_listings);
 
   return (
     <section className="px-4">
@@ -55,20 +63,28 @@ function SimilarProduct({ data, otherVendorData }) {
       <div className="grid grid-cols-2 -m-1.5 py-4">
         {similar_listings && similar_listings.length > 0 ? (
           similar_listings.map((item) => (
-            <div className="m-1.5" key={item.listingId}
-            onClick={() => {
-              // setListingId(item.listingId);
-              setProductsData(similar_listings || []);
-            }}
+            <div
+              className="m-1.5"
+              key={item.listingId}
+              onClick={() => {
+                // setListingId(item.listingId);
+                setProductsData(similar_listings || []);
+              }}
             >
-              <OtherListingCard data={item} setProducts={setSimilar_listings} prodLink />
+              <OtherListingCard
+                data={item}
+                setProducts={setSimilar_listings}
+                prodLink
+              />
             </div>
           ))
         ) : (
           // <div className="col-span-4">
           // <Loader />
           // </div>
-          <div className="text-center pt-2 col-span-4 h-20">There are no similar products</div>
+          <div className="text-center pt-2 col-span-4 h-20">
+            There are no similar products
+          </div>
         )}
       </div>
     </section>
