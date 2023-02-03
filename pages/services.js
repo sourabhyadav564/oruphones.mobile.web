9cd8852f5909bff0b3d003ef4151cad3991c43f4
Tollroad <nimits24@gmail.com>
2023-02-03 19:12:40 +0530

    login added price comparison updated

--- pages/services.js
+++ pages/services.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import service_img from "@/assets/service_img.png";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { servicesData } from "@/utils/constant";
 import Footer from "@/components/Footer";
 import VerifyFlowPopup from "@/components/Popup/VerifyFlowPopup";
@@ -8,9 +8,31 @@ import BottomNav from "@/components/Navigation/BottomNav";
 import priceComparison from "./services/PriceComparison";
 import PriceComparison from "./services/PriceComparison";
 import Link from "next/link";
+import LoginPopup from "@/components/Popup/LoginPopup";
+import { useAuthState } from "providers/AuthProvider";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 
 function Services() {
   const [openApp, setOpenApp] = useState(false);
+  const [openLoginPopup, setOpenLoginPopup] = useState(false);
+  const { authenticated, loading, user } = useAuthState();
+  const [performAction, setPerformAction] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const interval = setInterval(() => {
+    if (openLoginPopup == false && performAction == true && Cookies.get("userUniqueId") != undefined) {
+      clearInterval(interval);
+      router.push("/services/PriceComparison");
+    }
+  }, 1000);
+  }, [openLoginPopup]);
+
+  const handleClick = () => {
+    setOpenLoginPopup(true);
+    setPerformAction(true);
+  };
+
   return (
     <Fragment>
       <div className="p-4 text-lg bg-primary font-Roboto-Regular text-white text-center rounded-b-2xl">
@@ -28,13 +50,22 @@ function Services() {
       </div>
       <main className="px-3 my-4 font-Roboto-Regular grid">
         {servicesData.map((item, index) => (
-          <Link href={{ pathname: item.link || "" }}>
+          <Link href={{ pathname: user && item.link ? item?.link : "" }}>
             <div
               key={index}
-              className={`${item.link ? "border bg-[rgba(0,0,0,0.03)] py-2 px-4 pl-0 flex items-center font-Roboto-Medium rounded shadow mb-3" :
-               "border py-2 px-4 pl-0 flex items-center font-Roboto-Medium rounded shadow mb-3 opacity-60"}`}
+              className={`${
+                item.link
+                  ? "border bg-[rgba(0,0,0,0.03)] py-2 px-4 pl-0 flex items-center font-Roboto-Medium rounded shadow mb-3"
+                  : "border py-2 px-4 pl-0 flex items-center font-Roboto-Medium rounded shadow mb-3 opacity-60"
+              }`}
               data-aos="flip-up"
-              onClick={() => (item.link ? setOpenApp(false) : setOpenApp(true))}
+              onClick={() =>
+                item.link
+                  ? user
+                    ? setOpenApp(false)
+                    : handleClick()
+                  : setOpenApp(true)
+              }
             >
               <>
                 <div className="p-4">
@@ -58,7 +89,7 @@ function Services() {
       <Footer />
       <BottomNav />
       <VerifyFlowPopup open={openApp} setOpen={setOpenApp} />
-      <PriceComparison />
+      <LoginPopup open={openLoginPopup} setOpen={setOpenLoginPopup} />
     </Fragment>
   );
 }
